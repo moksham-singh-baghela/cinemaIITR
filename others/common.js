ff3abbfd81187ac4c2cd5d95cdce6c3232075153
moksham-singh-baghela <mokshambaghela@gmail.com>
2023-04-11 20:15:48 +0530

    update mobile css

---
+++ others/common.js
@@ -0,0 +1,5 @@
+function openbar() {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("menu").style.width = "100px";
+    document.getElementById("menu").style.backgroundColor = "#000000";
+}
